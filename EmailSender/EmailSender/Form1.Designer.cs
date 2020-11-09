2e068f650b5d7c7a96af72e1436f66ac0ced78ca
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2020-11-09 15:38:54 +0200

    Add files via upload

---
+++ EmailSender/EmailSender/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace EmailSender
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.emailText = new System.Windows.Forms.TextBox();
+            this.subjectText = new System.Windows.Forms.TextBox();
+            this.messageText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(36, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(113, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.panel1.Controls.Add(this.emailText);
+            this.panel1.Location = new System.Drawing.Point(113, 80);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(488, 39);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.GrayText;
+            this.panel2.Controls.Add(this.subjectText);
+            this.panel2.Location = new System.Drawing.Point(113, 156);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(488, 36);
+            this.panel2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(36, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Subject:";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.GrayText;
+            this.panel3.Controls.Add(this.messageText);
+            this.panel3.Location = new System.Drawing.Point(113, 231);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(488, 105);
+            this.panel3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(36, 242);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Message:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GrayText;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(473, 353);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 44);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // emailText
+            // 
+            this.emailText.AccessibleName = "email";
+            this.emailText.Location = new System.Drawing.Point(3, 11);
+            this.emailText.Name = "emailText";
+            this.emailText.Size = new System.Drawing.Size(482, 20);
+            this.emailText.TabIndex = 8;
+            // 
+            // subjectText
+            // 
+            this.subjectText.AccessibleName = "subjectText";
+            this.subjectText.Location = new System.Drawing.Point(3, 8);
+            this.subjectText.Name = "subjectText";
+            this.subjectText.Size = new System.Drawing.Size(482, 20);
+            this.subjectText.TabIndex = 9;
+            this.subjectText.Tag = "subjectText";
+            // 
+            // messageText
+            // 
+            this.messageText.AccessibleName = "messageText";
+            this.messageText.Location = new System.Drawing.Point(3, 3);
+            this.messageText.Multiline = true;
+            this.messageText.Name = "messageText";
+            this.messageText.Size = new System.Drawing.Size(482, 98);
+            this.messageText.TabIndex = 10;
+            this.messageText.Tag = "messageText";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(32, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(311, 39);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Email Sender in C#";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(702, 456);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox emailText;
+        private System.Windows.Forms.TextBox subjectText;
+        private System.Windows.Forms.TextBox messageText;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ EmailSender/EmailSender/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+//using System.Net.Mail.SmtpClient;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmailSender
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SmtpClient Client = new SmtpClient()
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential()
+                {
+                UserName = "",
+                Password = ""
+                }
+            };
+            
+
+            MailAddress FromEmail = new MailAddress("windoshuni@gmail.com", "Kovacs Balint Hunor");
+            MailAddress ToEmail = new MailAddress(emailText.Text, "Someone");
+            MailMessage Message = new MailMessage()
+            {
+                From = FromEmail,
+                Subject=subjectText.Text,
+                Body = messageText.Text
+            };
+            ///messsage sending
+            Message.To.Add(ToEmail);
+            ///outdated errorcheck solution
+            
+            /*try{
+            Client.Send(Message);
+            MessageBox.Show("Sent Successfully", "Done");
+            
+            }
+            catch(Exception ex){
+                MessageBox.Show("Something wrong\n" + ex.InnerException.Message, "Error");
+
+            }*/
+            Client.SendCompleted += Client_SendCompleted;    
+            Client.SendMailAsync(Message);
+        }
+        /// error checks
+        private void Client_SendCompleted(object sender, AsyncCompletedEventArgs e) 
+        {
+            if (e.Error != null) 
+            {
+                MessageBox.Show("Error Happening \n" + e.Error.Message, "Error");
+                return;
+            }
+            MessageBox.Show("Sent Successfully", "Done");
+        }
+    }
+}
